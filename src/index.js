1b75f0aa3468a2fff53e710aafbc4f1f91158617
Maxpayne <ferhatadibelli9@gmail.com>
2022-05-19 12:10:06 +0300

    updated

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.Fragment>
-    <BrowserRouter basename={process.env.PUBLİC_URL}>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.Fragment>,
