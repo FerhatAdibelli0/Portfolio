b9f2f1662a3d9897115225f6c38fd989a7d6cd85
Maxpayne <ferhatadibelli9@gmail.com>
2022-05-19 10:21:55 +0300

    homegape url added

--- src/App.js
+++ src/App.js
@@ -30,10 +30,10 @@ function App() {
         <Navbar />
         <ScrollToTop />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/project" element={<Projects />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/resume" element={<Resume />} />
+          <Route path="/Portfolio" element={<Home />} />
+          <Route path="/Portfolio/project" element={<Projects />} />
+          <Route path="/Portfolio/about" element={<About />} />
+          <Route path="/Portfolio/resume" element={<Resume />} />
           <Route path="*" element={<Navigate to="/" />} />
         </Routes>
         <Footer />
