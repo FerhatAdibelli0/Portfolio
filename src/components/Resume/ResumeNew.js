c348b75c9113671f7773f3d2bc17ca005b9ac736
Maxpayne <ferhatadibelli9@gmail.com>
2022-05-21 00:26:46 +0300

    updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/myResume.pdf";
+import pdf from "../../Assets/../Assets/myResume1.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/FerhatAdibelli0/Portfolio/master/src/Assets/myResume.pdf";
+  "https://raw.githubusercontent.com/FerhatAdibelli0/Portfolio/master/src/Assets/myResume1.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -36,7 +36,7 @@ function ResumeNew() {
 
         <Row className="resume">
           <Document file={resumeLink} className="d-flex justify-content-center">
-            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+            <Page pageNumber={2} scale={width > 786 ? 1.7 : 0.6} />
           </Document>
         </Row>
 
