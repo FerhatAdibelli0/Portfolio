e08366938eade6c6cf1c733a264e593b0d581460
Maxpayne <ferhatadibelli9@gmail.com>
2022-05-21 01:30:15 +0300

    updated

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/myResume1.pdf";
+import pdf from "../../Assets/../Assets/myResume2.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://raw.githubusercontent.com/FerhatAdibelli0/Portfolio/master/src/Assets/myResume1.pdf";
+  "https://raw.githubusercontent.com/FerhatAdibelli0/Portfolio/master/src/Assets/myResume2.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
